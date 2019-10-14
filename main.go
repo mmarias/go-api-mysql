9cbea8f23ee99126e83e4ec9383d4a39d9cc9b06
Martín Arias <mmarias.rm@gmail.com>
2019-10-14 09:13:55 -0300

    add methods

--- main.go
+++ main.go
@@ -4,15 +4,27 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	//"io/ioutil"
+	"io/ioutil"
 	"database/sql"
 	"encoding/json"
 	"github.com/gorilla/mux"
+	"golang.org/x/crypto/bcrypt"
 	_"github.com/go-sql-driver/mysql"
 )
 
+type ApiResponse struct {
+	Result string `json:"result"`
+}
+
+type User struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+	Status string `json:"status"`
+	Sex string `json:"sex"`
+}
+
 func getUsers(w http.ResponseWriter, r *http.Request) {
-	var users interface{}
+	var users []User
 
 	db, err := sql.Open("mysql", "root:12345678@tcp(localhost:3306)/go")
 	if err != nil {
@@ -20,20 +32,78 @@ func getUsers(w http.ResponseWriter, r *http.Request) {
 	} else {
 		fmt.Println("db is connected")
 	}
+
 	defer db.Close()
 
-	resp, err := db.Prepare("SELECT * FROM user")
+	resp, err := db.Query("SELECT username, password, status, sex FROM user")
+
 	if err != nil {
 		fmt.Println(err)
 	}
 	defer resp.Close()
 	
-	json.NewEncoder(w).Encode(resp)
-	fmt.Println(resp)
+	for resp.Next() {
+		var user User
+
+		err = resp.Scan(&user.Username, &user.Password, &user.Status, &user.Sex)
+        if err != nil {
+            panic(err.Error()) // proper error handling instead of panic in your app
+		}
+
+		users = append(users, user)
+	}
+
+	json.NewEncoder(w).Encode(users)
 }
 
 func addUser(w http.ResponseWriter, r *http.Request) {
+	db, err := sql.Open("mysql", "root:12345678@tcp(localhost:3306)/go")
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(ApiResponse{"Error on connecto to DB"})
+	}
+
+	defer db.Close()
+
+	reqBody, err := ioutil.ReadAll(r.Body)
+	
+
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(ApiResponse{"Error on request parameters"})
+	}
+
+	var newUser User
+
+	json.Unmarshal(reqBody, &newUser)
+	
+	if newUser.Username == "" || newUser.Password == "" || newUser.Status == "" || newUser.Sex == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(ApiResponse{"Error on request parameters"})
+		return
+	}
+
+	hash, err := bcrypt.GenerateFromPassword([]byte(newUser.Password), bcrypt.DefaultCost)
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(ApiResponse{"Error on hash password"})
+	}
+
+	insert, err := db.Query(""+
+	"INSERT INTO user (username, password, status, sex)"+
+	"VALUES ('"+newUser.Username+"','"+string(hash)+"','"+newUser.Status+"','"+newUser.Sex+"')")
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(ApiResponse{"Error on create user"})
+	}
+
+	defer insert.Close()
+
 	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(ApiResponse{"User created"})
 }
 
 func main() {
